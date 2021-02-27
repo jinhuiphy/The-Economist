16f265333387249c52fe92f071417a8ad2114243
jinhuiphy <jinhuiphy@outlook.com>
2021-02-27 11:08:02 +0800

    update to 2021

--- audioDownload.py
+++ audioDownload.py
@@ -4,14 +4,14 @@ from datetime import datetime
 
 #%%
 def get_url(year, date, issue):
-    # year = 2020, date = 20200620, issue = 9199
     base_url = "http://audiocdn.economist.com/sites/default/files/AudioArchive/"
     url = base_url + "{Year}/{Date}/Issue_{Issue}_{Date}_The_Economist_Full_edition.zip".format(Year = year, Date = date, Issue = issue)
     return url
 
 def get_issue(date):
-    base_issue = 9199
-    base_date = datetime(2020, 6, 20)
+    # year = 2020, date = 20200620, issue = 9199
+    base_issue = 9226
+    base_date = datetime(2021, 1, 2)
     current_date = datetime.strptime(date, "%Y%m%d")
     return int(base_issue + (current_date - base_date).days / 7)
 
@@ -21,7 +21,7 @@ def return_dirs(path):
     r_dirs = []
     for root, dirs, _ in os.walk(path):
         for name in dirs:
-            if name.startswith("2020"):
+            if name.startswith("2021"):
                 r_dirs.append(os.path.join(root,name))
         break
     return r_dirs
@@ -46,7 +46,7 @@ for dir in dir_list:
             else:
                 print("Path {} already exist!".format(audio_path))
             # Audio zip file url
-            audio_url = get_url(year=2020, date=date, issue=get_issue(date))
+            audio_url = get_url(year=2021, date=date, issue=get_issue(date))
             print("Preparing download file with url below:\n{}".format(audio_url))
             print("Download begin, please wait!")
             r = requests.get(audio_url)
